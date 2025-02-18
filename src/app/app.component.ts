d09b9d8bc6fbffea0899d0371eda202d61c8c01a
Foucauld Bellanger <63885990+Foukki@users.noreply.github.com>
2025-02-18 16:41:04 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import * as data from './shared/assets/data/questions_final.json';
+import { QuizData } from './shared/types/interfaces';
+import { NavbarComponent } from './views/navbar/navbar.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [CommonModule, RouterOutlet, NavbarComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'INCLUSIF: Le jeu';
+  quizData = quizData;
+}
+
+export const quizData: QuizData = data;
