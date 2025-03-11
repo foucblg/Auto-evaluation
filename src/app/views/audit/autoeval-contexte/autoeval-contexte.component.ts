ae98eb6306d535d56a2895a559a118c6307cbc85
Foucauld Bellanger <63885990+Foukki@users.noreply.github.com>
2025-03-11 17:09:39 +0100

    waiting for Quentin

--- src/app/views/audit/autoeval-contexte/autoeval-contexte.component.ts
+++ src/app/views/audit/autoeval-contexte/autoeval-contexte.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import {Router } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { ProgressService } from '../../../shared/services/progress-service';
+import { Divider } from 'primeng/divider';
 
 @Component({
   selector: 'app-autoeval-contexte',
   templateUrl: './autoeval-contexte.component.html',
   styleUrl: './autoeval-contexte.component.css',
-  imports: [ButtonModule]
+  imports: [ButtonModule, Divider]
 })
 export class ContexteComponent {
   constructor(private router:Router, private progressService:ProgressService) {}
